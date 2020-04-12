bbfc342b8d88d2207c9893f19116f20bb66f9b46
andres <arobert@acode.com>
2020-04-12 12:03:22 -0400

    add follow scroll menu

--- index.php
+++ index.php
@@ -15,17 +15,17 @@
         <li><a href="https://github.com/AndresRobert/Simple-Framework" target="_blank"><i class="fab fa-github"></i></a></li>
     </ul>
 </header>
-<nav>
+<nav class="followScroll">
     <a href="#grid" class="active"><i class="fas fa-grip-horizontal"></i>Grid</a>
-    <a href="#buttons"><i class="fas fa-mouse-pointer"></i>Buttons</a>
     <a href="#cards"><i class="fas fa-address-card"></i>Cards</a>
-    <a href="#tables"><i class="fas fa-table"></i>Tables</a>
-    <a href="#dialog"><i class="fas fa-window-restore"></i>Dialogs</a>
     <a href="#typo"><i class="fas fa-heading"></i>Typo</a>
-    <a href="#gallery"><i class="fas fa-images"></i>Gallery</a>
+    <a href="#tables"><i class="fas fa-table"></i>Tables</a>
     <a href="#lists"><i class="fas fa-list-ul"></i>Lists</a>
+    <a href="#gallery"><i class="fas fa-images"></i>Gallery</a>
     <a href="#navigation" class="hideOnSmall"><i class="fas fa-bars"></i>Navigation</a>
+    <a href="#dialog"><i class="fas fa-window-restore"></i>Dialogs</a>
     <a href="#progress"><i class="fas fa-spinner"></i>Progress</a>
+    <a href="#buttons"><i class="fas fa-mouse-pointer"></i>Buttons</a>
 </nav>
 <main class="pv-4 hasNav">
     <div id="grid" class="grid">
@@ -91,43 +91,6 @@
             <div class="col-1 border center">1</div>
         </div>
     </div>
-    <div id="buttons" class="grid">
-        <h3>Buttons</h3>
-        <p class="title">This style applies to button and a.button</p>
-        <div class="row">
-            <div class="col-4 center">
-                <button><i class="fas fa-star"></i>button</button>
-            </div>
-            <div class="col-4 center">
-                <button class="fill"><i class="fas fa-star"></i>button .fill</button>
-            </div>
-            <div class="col-4 center">
-                <button class="text"><i class="fas fa-star"></i>button .text</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-4 center">
-                <button class="square"><i class="fas fa-star"></i>button .square</button>
-            </div>
-            <div class="col-4 center">
-                <button class="round fill"><i class="fas fa-star"></i>.round.fill</button>
-            </div>
-            <div class="col-4 center">
-                <button disabled><i class="fas fa-times"></i>disabled</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-4 center">
-                <button class="fab"><i class="fas fa-plus"></i></button>
-            </div>
-            <div class="col-4 center">
-                <button class="fab fill"><i class="fas fa-pen"></i></button>
-            </div>
-            <div class="col-4 center">
-                <button class="fab text"><i class="fas fa-link"></i></button>
-            </div>
-        </div>
-    </div>
     <div id="cards" class="grid">
         <h3>Cards</h3>
         <div class="row">
@@ -170,6 +133,23 @@
                         <button class="text icon"><i class="fas fa-ellipsis-v"></i></button>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div id="typo" class="grid">
+        <h3>Typo & Dividers</h3>
+        <p class="title">Uses Open Sans out from the box (300 & 600 only, but you can add more if needed)</p>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="paddingless">Header 1</h1>
+                <h2 class="paddingless">Header 2</h2>
+                <h3 class="paddingless">Header 3</h3>
+                <h4 class="paddingless">Header 4</h4>
+                <h5 class="paddingless">Header 5</h5>
+                <h6 class="paddingless">Header 6</h6>
+                <p>Paragraph</p>
+                <hr>
+                <p>Divider</p>
             </div>
         </div>
     </div>
@@ -223,109 +203,6 @@
                     </tr>
                     </tbody>
                 </table>
-            </div>
-        </div>
-    </div>
-    <div id="dialog" class="grid">
-        <h3>Dialogs, Sheets & Snackbar</h3>
-        <div class="row">
-            <div class="col-12">
-                <button onclick="
-                    $.dialog(
-                        'Dialog body very, very, long text example for testing purposes',
-                        {
-                            title: 'Dialog Header',
-                            buttons: [
-                                { text: 'Action 1', id: 'action1' },
-                                { text: 'Action 2', id: 'action2' }
-                            ]
-                        }
-                    )">Show Dialog</button>
-                <button onclick="
-                    $.sheet(
-                        'bottom',
-                        '<h5>Bottom Sheet</h5><p class=\'title\'>It\'s a card! you can add html on it!</p><p>And any Text you want.</p>'
-                    )">Bottom Sheet</button>
-                <button onclick="
-                    $.sheet(
-                        'side',
-                        '<h5>Side Sheet</h5><p class=\'title\'>It\'s a card! you can add html on it!</p><p>And any Text you want.</p>'
-                    )">Side Sheet</button>
-                <button onclick="$.snackbar('This will be dismissed in 3 secs')">Timed Snackbar</button>
-                <button onclick="$.snackbar('This will stay here until you click on: ', 'Dismiss')">Snackbar</button>
-            </div>
-        </div>
-    </div>
-    <div id="typo" class="grid">
-        <h3>Typo & Dividers</h3>
-        <p class="title">Uses Open Sans out from the box (300 & 600 only, but you can add more if needed)</p>
-        <div class="row">
-            <div class="col-12">
-                <h1 class="paddingless">Header 1</h1>
-                <h2 class="paddingless">Header 2</h2>
-                <h3 class="paddingless">Header 3</h3>
-                <h4 class="paddingless">Header 4</h4>
-                <h5 class="paddingless">Header 5</h5>
-                <h6 class="paddingless">Header 6</h6>
-                <p>Paragraph</p>
-                <hr>
-                <p>Divider</p>
-            </div>
-        </div>
-    </div>
-    <div id="gallery" class="grid">
-        <h3>Image Gallery</h3>
-        <div class="row">
-            <div class="col-12 hideOnSmall">
-                <div class="gallery">
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                        <p>Image Info <i class="far fa-star"></i></p>
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example2.jpg" alt="">
-                    </div>
-                    <div class="item round">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 hideOnSmall">
-                <div class="gallery six">
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                        <p>Image Info <i class="far fa-star"></i></p>
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example2.jpg" alt="">
-                    </div>
-                    <div class="item round">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="gallery two">
-                    <div class="item">
-                        <img src="src/img/example.jpg" alt="">
-                        <p>Image Info <i class="far fa-star"></i></p>
-                    </div>
-                    <div class="item round">
-                        <img src="src/img/example2.jpg" alt="">
-                    </div>
-                </div>
             </div>
         </div>
     </div>
@@ -437,6 +314,62 @@
             </div>
         </div>
     </div>
+    <div id="gallery" class="grid">
+        <h3>Image Gallery</h3>
+        <div class="row">
+            <div class="col-12 hideOnSmall">
+                <div class="gallery">
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                        <p>Image Info <i class="far fa-star"></i></p>
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example2.jpg" alt="">
+                    </div>
+                    <div class="item round">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 hideOnSmall">
+                <div class="gallery six">
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                        <p>Image Info <i class="far fa-star"></i></p>
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example2.jpg" alt="">
+                    </div>
+                    <div class="item round">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="gallery two">
+                    <div class="item">
+                        <img src="src/img/example.jpg" alt="">
+                        <p>Image Info <i class="far fa-star"></i></p>
+                    </div>
+                    <div class="item round">
+                        <img src="src/img/example2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="navigation" class="grid hideOnSmall">
         <h3>Navigation</h3>
         <p class="title">Only affects desktop view, on mobile is always collapsed. If Rail is on on mobile is always visible</p>
@@ -446,7 +379,41 @@
                 <button onclick="
                     $('nav').toggleClass('rail');
                     $('main').toggleClass('hasNav').toggleClass('hasRail');
-                    $('footer').toggleClass('hasNav').toggleClass('hasRail');">Toggle Rail View</button>
+                    $('footer').toggleClass('hasNav').toggleClass('hasRail');">Toggle Rail View
+                </button>
+            </div>
+        </div>
+    </div>
+    <div id="dialog" class="grid">
+        <h3>Dialogs, Sheets & Snackbar</h3>
+        <div class="row">
+            <div class="col-12">
+                <button onclick="
+                    $.dialog(
+                        'Dialog body very, very, long text example for testing purposes',
+                        {
+                            title: 'Dialog Header',
+                            buttons: [
+                                { text: 'Action 1', id: 'action1' },
+                                { text: 'Action 2', id: 'action2' }
+                            ]
+                        }
+                    )">Show Dialog
+                </button>
+                <button onclick="
+                    $.sheet(
+                        'bottom',
+                        '<h5>Bottom Sheet</h5><p class=\'title\'>It\'s a card! you can add html on it!</p><p>And any Text you want.</p>'
+                    )">Bottom Sheet
+                </button>
+                <button onclick="
+                    $.sheet(
+                        'side',
+                        '<h5>Side Sheet</h5><p class=\'title\'>It\'s a card! you can add html on it!</p><p>And any Text you want.</p>'
+                    )">Side Sheet
+                </button>
+                <button onclick="$.snackbar('This will be dismissed in 3 secs')">Timed Snackbar</button>
+                <button onclick="$.snackbar('This will stay here until you click on: ', 'Dismiss')">Snackbar</button>
             </div>
         </div>
     </div>
@@ -509,6 +476,43 @@
             </div>
         </div>
     </div>
+    <div id="buttons" class="grid">
+        <h3>Buttons</h3>
+        <p class="title">This style applies to button and a.button</p>
+        <div class="row">
+            <div class="col-4 center">
+                <button><i class="fas fa-star"></i>button</button>
+            </div>
+            <div class="col-4 center">
+                <button class="fill"><i class="fas fa-star"></i>button .fill</button>
+            </div>
+            <div class="col-4 center">
+                <button class="text"><i class="fas fa-star"></i>button .text</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4 center">
+                <button class="square"><i class="fas fa-star"></i>button .square</button>
+            </div>
+            <div class="col-4 center">
+                <button class="round fill"><i class="fas fa-star"></i>.round.fill</button>
+            </div>
+            <div class="col-4 center">
+                <button disabled><i class="fas fa-times"></i>disabled</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4 center">
+                <button class="fab"><i class="fas fa-plus"></i></button>
+            </div>
+            <div class="col-4 center">
+                <button class="fab fill"><i class="fas fa-pen"></i></button>
+            </div>
+            <div class="col-4 center">
+                <button class="fab text"><i class="fas fa-link"></i></button>
+            </div>
+        </div>
+    </div>
 </main>
 <footer class="hasNav">
     <div id="" class="grid">
@@ -530,12 +534,12 @@
 <script src="src/acode.0.1.0.js?v=v=0.1.4"></script>
 <script src="https://kit.fontawesome.com/a93fffc8fc.js" crossorigin="anonymous"></script>
 <script>
-    const showAsyncProgressBar = time => {
+    let showAsyncProgressBar = time => {
         $.loading.async();
         setTimeout(() => $.loading.dismiss(), time * 1000)
     };
 
-    const showSyncProgressBar = time => {
+    let showSyncProgressBar = time => {
         $.loading.sync();
         setTimeout(() => $.loading.dismiss(), time * 1000)
     };
--- src/acode.0.1.0.js
+++ src/acode.0.1.0.js
@@ -2,9 +2,12 @@
 
 $ = sel => {
     const el = document.querySelectorAll(sel);
-    return (el.length === 1) ? el[0] : el.length === 0 ? null : el;
+    return el.length === 0 ? null : el[0];
 };
-$$ = sel => document.querySelectorAll(sel);
+$$ = sel => {
+    const el = document.querySelectorAll(sel);
+    return (el.length === 0) ? null : el;
+};
 $.ready = fn => {
     if (document.readyState !== 'loading') fn();
     else document.addEventListener('DOMContentLoaded', fn);
@@ -105,12 +108,10 @@ $(body).append(
 */
 
 $.attributeExceptions = ['role'];
-
 $.appendText = (el, text) => {
     const textNode = document.createTextNode(text);
     el.appendChild(textNode);
 };
-
 $.appendArray = (el, children) => {
     children.forEach((child) => {
         if (Array.isArray(child)) $.appendArray(el, child);
@@ -118,7 +119,6 @@ $.appendArray = (el, children) => {
         else if (typeof child === 'string') $.appendText(el, child);
     });
 };
-
 $.setStyles = (el, styles) => {
     if (!styles) {
         el.removeAttribute('styles');
@@ -133,7 +133,6 @@ $.setStyles = (el, styles) => {
         }
     });
 };
-
 $.makeElement = (type, textOrPropsOrChild, ...children) => {
     const el = document.createElement(type);
     if (Array.isArray(textOrPropsOrChild)) $.appendArray(el, textOrPropsOrChild);
@@ -235,7 +234,7 @@ const SyncLoadingCircle = () => {
  * @param item
  * @returns {boolean}
  */
-const isset = item => {
+$.isset = item => {
     return typeof item !== 'undefined' && item !== null;
 };
 
@@ -252,26 +251,26 @@ const isset = item => {
  * @returns {Promise}
  *
  */
-async function ajax (url = '', data = {}, options = {}) {
-    const method = isset(options.method)
+$.ajax = async function (url = '', data = {}, options = {}) {
+    const method = $.isset(options.method)
         ? options.method
         : { 'Content-Type': 'application/json' }; // *GET, POST, PUT, DELETE, etc.
-    const contentType = isset(options.content_type)
+    const contentType = $.isset(options.content_type)
         ? options.content_type
         : { 'Content-Type': 'application/json' }; // 'Content-Type': 'application/x-www-form-urlencoded'
-    const cors = isset(options.cors)
+    const cors = $.isset(options.cors)
         ? options.cors // no-cors, *cors, same-origin
         : 'cors';
-    const cache = isset(options.cache)
+    const cache = $.isset(options.cache)
         ? options.cache // *default, no-cache, reload, force-cache, only-if-cached
         : 'no-cache';
-    const credentials = isset(options.credentials)
+    const credentials = $.isset(options.credentials)
         ? options.credentials // include, *same-origin, omit
         : 'same-origin';
-    const redirect = isset(options.redirect)
+    const redirect = $.isset(options.redirect)
         ? options.redirect // manual, *follow, error
         : 'follow';
-    const policy = isset(options.policy)
+    const policy = $.isset(options.policy)
         ? options.policy // no-referrer, *client
         : 'client';
     const response = await fetch(url, {
@@ -285,16 +284,16 @@ async function ajax (url = '', data = {}, options = {}) {
         body: JSON.stringify(data) // body data type must match 'Content-Type' header
     });
     return await response.json(); // parses JSON response into native JavaScript objects
-}
+};
 
-setCookie = (name, value, days = 90) => {
+$.cookie = {};
+$.cookie.set = (name, value, days = 90) => {
     let date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     let expires = "; expires=" + date.toUTCString();
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 };
-
-getCookie = name => {
+$.cookie.get = name => {
     let nameEQ = name + "=";
     let ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
@@ -304,18 +303,17 @@ getCookie = name => {
     }
     return null;
 };
-
-eraseCookie = name => document.cookie = name + '=; Max-Age=-99999999;';
+$.cookie.delete = name => document.cookie = name + '=; Max-Age=-99999999;';
 
 // Framework
 
 $.dialog = (message = '', options = {}) => {
-    let title = isset(options.title) ? options.title : 'Information',
-        buttons = isset(options.buttons) ? options.buttons : [],
+    let title = $.isset(options.title) ? options.title : 'Information',
+        buttons = $.isset(options.buttons) ? options.buttons : [],
         _buttons = [];
     buttons.forEach(_button => {
-        let _id = isset(_button.id) ? _button.id : '',
-            _text = isset(_button.text) ? _button.text : '';
+        let _id = $.isset(_button.id) ? _button.id : '',
+            _text = $.isset(_button.text) ? _button.text : '';
         if (_text !== '') {
             _buttons.push(button({ className: 'text', id: _id }, _text));
         }
@@ -336,7 +334,7 @@ $.sheet = (type = '', html = '') => {
 };
 
 $.fullscreenImg = src => {
-    if (isset(src)) {
+    if ($.isset(src)) {
         Render(Gallery({ src: src }));
         $('#sfGallery').on('click', () => {
             $('#sfGallery').fadeRemove();
@@ -345,26 +343,22 @@ $.fullscreenImg = src => {
 };
 
 $.loading = {};
-
 $.loading.async = () => Render(AsyncLoadingBar());
-
 $.loading.sync = () => Render(SyncLoadingCircle());
-
 $.loading.dismiss = () => {
     let sfAsyncLoadingBar = $('#sfAsyncLoadingBar'),
         sfSyncLoadingCircle = $('#sfSyncLoadingCircle');
-    if (isset(sfAsyncLoadingBar)) sfAsyncLoadingBar.fadeRemove();
-    if (isset(sfSyncLoadingCircle)) sfSyncLoadingCircle.fadeRemove();
+    if ($.isset(sfAsyncLoadingBar)) sfAsyncLoadingBar.fadeRemove();
+    if ($.isset(sfSyncLoadingCircle)) sfSyncLoadingCircle.fadeRemove();
 };
 
 $.snackbarId = 1;
-
 $.snackbar = (message, button = '') => {
     let _id = $.snackbarId++,
     _container = $('.snackbarContainer');
     if (button === '') {
         _container.appendChild(div({ id: 'snackItem' + _id }, message));
-        let _time = message.length * 50 + 3000;
+        let _time = message.length * 20 + 3000;
         setTimeout(() => $('#snackItem' + _id).fadeRemove() , _time);
     }
     else {
@@ -372,6 +366,18 @@ $.snackbar = (message, button = '') => {
         let _snackbar = $('#snackItem' + _id);
         _snackbar.find('span')[0].on('click', () => _snackbar.fadeRemove());
     }
+};
+
+$.inView = (elm, threshold = 0) => {
+    const rect = elm.getBoundingClientRect();
+    const vpWidth = window.innerWidth;
+    const vpHeight = window.innerHeight;
+    const above = rect && rect.bottom - threshold <= 0;
+    const below = rect && rect.top - vpHeight + threshold >= 0;
+    const left = rect && rect.right - threshold <= 0;
+    const right = rect && rect.left - vpWidth + threshold >= 0;
+    const inside = !!rect && !above && !below && !left && !right;
+    return { inside, above, below, left, right };
 };
 
 $.init = component => {
@@ -384,6 +390,48 @@ $.init = component => {
                     e.target.addClass('active');
                     $('nav').removeClass('active');
                 });
+            });
+            break;
+        case 'followScroll':
+            let active = true;
+            window.addEventListener('scroll', () => {
+                const item = $('.followScroll').find('a'),
+                    first = 0,
+                    last = item.length - 1,
+                    scrollTop = ((document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop) === 0,
+                    scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight,
+                    scrolledToBottom = (((document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop) + window.innerHeight) >= scrollHeight;
+                console.log(window.innerHeight, scrollHeight);
+                if (scrollTop) {
+                    if (!item[first].hasClass('active')) {
+                        item.forEach(e => { e.removeClass('active') });
+                        item[first].addClass('active')
+                    }
+                }
+                else
+                    if (scrolledToBottom) {
+                        if (!item[last].hasClass('active')) {
+                            item.forEach(e => { e.removeClass('active') });
+                            item[last].addClass('active')
+                        }
+                    }
+                    else {
+                        item.forEach(e => {
+                            let { inside } = $.inView($(e.attr('href')));
+                            if (!active) {
+                                if (inside && !e.hasClass('active')) {
+                                    e.addClass('active');
+                                    active = true;
+                                }
+                            }
+                            else {
+                                if (!inside && e.hasClass('active')) {
+                                    e.removeClass('active');
+                                    active = false;
+                                }
+                            }
+                        });
+                    }
             });
             break;
         case 'snackbar':
@@ -450,6 +498,7 @@ $.init = component => {
 
 $.ready(() => {
     $.init('nav');
+    $.init('followScroll');
     $.init('snackbar');
     $.init('gallery');
     $.init('circleIndicator');
